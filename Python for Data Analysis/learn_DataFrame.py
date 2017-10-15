02387c7272facdf535bc856de63338daa4fcfb25
Sheila Chen <dandan.jhu@gmail.com>
2017-10-15 17:49:47 -0400

    some codes

--- Python for Data Analysis/learn_DataFrame.py
+++ Python for Data Analysis/learn_DataFrame.py
@@ -3,6 +3,20 @@
 """
 Created on Sun Oct 15 17:31:39 2017
 
+Examples from the Book Python for Data Analysis, on Data Frames
+
 @author: xuehuachen
 """
 
+from pandas import DataFrame
+import pandas as pd
+import pandas.io.data as web
+
+all_data = {}
+for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']:
+    all_data[ticker] = web.get_data_google(ticker)
+
+price = DataFrame({tic: data['Close'] for tic, data in all_data.iteritems()})
+volume = DataFrame({tic: data['Volume'] for tic, data in all_data.iteritems()})
+
+returns = price.pct_change()
